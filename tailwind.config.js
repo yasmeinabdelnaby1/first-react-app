99b10493574539b7247669e93bf6a877b8aa11c1
yasmeinabdelnaby1 <yasmeinabdalnaby@gmail.com>
2025-06-23 23:25:04 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+    screens: {
+      xs:"320px" ,
+      sm:"375px" ,
+      sml:"500px" ,
+      md:"667px" ,
+      mdl:"768px" ,
+      lg:"660px" ,
+      lgl:"1024px" ,
+      xl:"1280px" ,
+      
+      },
+      container: {
+        center: true,
+      },
+  
+    
+    extend: {},
+  },
+  plugins: [],
+}
